481318852fbb565dd3844ed78b154998d17e5f84
Sublio <sublio1260@gmail.com>
2017-06-29 17:40:19 +0600

    CustomException testing

---
+++ customException.rb
@@ -0,0 +1,5 @@
+
+class customExceptionError < StandardError
+end
+
+raise customExceptionError
--- dummy.rb
+++ dummy.rb
@@ -4,30 +4,37 @@ require 'rest-client'
 
 
 
-caps = Appium.load_appium_txt file: File.join(Dir.pwd, 'appium.txt') #get caps from external txt file
+  caps = Appium.load_appium_txt file: File.join(Dir.pwd, 'appium.txt') #get caps from external txt file
+
 
 
   # Start the driver
   @driver = Appium::Driver.new(caps)
   @driver.start_driver
 
-  sleep(2);
 
 
 module Dummy
   module IOS
     Appium.promote_singleton_appium_methods Dummy
 
+
+
+    textField = textfields
     buttonsAll = buttons
-    
-    buttonsAll[1].click()
+    textSum = first_text
+    textField[0].type("1")
+    textField[1].type("2")
 
-    puts server_url
-    exists { buttonsAll[1]} ? puts('true') : puts('false')
+    sumButton = find_element(:name =>"Compute Sum")
+    sumButton.click
 
-    puts ios_version
+    puts textSum.text
+
+    raise Exception, "Bitch please" if textSum.text != "3"
 
     #driver_quit
     x
+
   end
 end
