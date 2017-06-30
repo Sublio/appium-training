e2e1b6181d775aaa666c0071030b64831994d7c0
Sublio <sublio1260@gmail.com>
2017-06-30 15:06:04 +0600

    Some refactor

---
+++ login_tests.rb
@@ -0,0 +1,42 @@
+require 'rubygems'
+require 'appium_lib'
+require 'test/unit'
+require_relative 'ChunkyImageCompareService'
+
+
+# For Launching test install Bundle and then "bundle install & bundle exec ruby login_tests.rb"
+
+
+  caps = Appium.load_appium_txt file: File.join(Dir.pwd, 'appiumiOSCaps.txt') #get caps from external txt file
+
+
+class LoginTests < Test::Unit::TestCase
+
+
+
+  def setup
+
+    caps = Appium.load_appium_txt file: File.join(Dir.pwd, 'appiumiOSCaps.txt')
+
+    driver = Appium::Driver.new(caps)
+    Appium.promote_appium_methods self.class
+    driver.start_driver
+
+  end
+
+
+  def teardown
+    driver_quit
+  end
+
+
+
+  def testMethod
+
+    button(1).click
+    sleep(5)
+  end
+
+
+
+end
