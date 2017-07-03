96748056800ea3623bab814cc03bcd82093edd79
Sublio <sublio1260@gmail.com>
2017-07-03 15:07:40 +0600

    User data depot

---
+++ user_data_depot.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'appium_lib'
+
+
+
+class UserDataDepot
+
+
+  def self.arrayOfValidMailsPasswords
+
+
+      validUserMailPassword = [{'mail' =>'b@b.b', 'password' => 'qweasdzxc'},
+                               {'mail' => 'leon@rambler.com	', 'password' => 'qweasdzxc'},
+                               {'mail' => 'andrey@ko.com', 'password' => 'qweasdzxc'},
+                               {'mail' => 'prodavets@mail.com','password' => 'qweasdzxc'}]
+
+  end
+
+end
+
+
+#@collection = [{ "firstname" => "Mark", "lastname" => "Martin", "age" => "24", "gender" => "M" }]
