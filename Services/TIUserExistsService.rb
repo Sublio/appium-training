7908107a5755372da29fcdc3849a7d0d34560ee2
Sublio <sublio1260@gmail.com>
2017-07-03 14:39:13 +0600

    User exists service

---
+++ Services/TIUserExistsService.rb
@@ -0,0 +1,15 @@
+require 'rest-client'
+
+
+class TIUserExistsService
+
+  BASE_URL_CONTS = "https://api-dev.thetrustedinsight.com/api/"
+
+  def self.checkUserExists(usermail, userpassword)
+
+
+
+  end
+
+
+end
