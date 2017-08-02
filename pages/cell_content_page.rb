eb1bfbd79a9f03824d6702687b3c05d109a4f5ce
Sublio <sublio1260@gmail.com>
2017-08-02 15:48:05 +0600

    cell content pager

---
+++ pages/cell_content_page.rb
@@ -0,0 +1,50 @@
+
+class CELLCONTENT_PAGE
+
+  attr_accessor :driver
+
+  def initialize (driver)
+    @driver = driver
+    Appium.promote_appium_methods self.class
+  end
+
+
+  def backButton
+
+    if @driver.device_is_android?
+
+      @driver.find_element(:id, "Navigate up")
+
+    else
+
+      wait { @driver.find_element(:name, 'iconTopBack') }
+
+    end
+
+  end
+
+
+  def addToBookmarkButton
+
+
+    if @driver.device_is_android? #case android
+
+
+      wait { @driver.find_element(:id, "com.thetrustedinsight.tiapp:id/op_bookmark")}
+
+
+
+    else #case ios
+
+      wait { @driver.find_element(:name, 'iconNavBookmarkEmpty') }
+
+    end
+
+  end
+
+  def topNavigationBar
+
+
+  end
+
+end
