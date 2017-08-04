8f3ba162998210fe97d4c53e8964f1dfa547c045
Sublio <sublio1260@gmail.com>
2017-08-04 15:38:41 +0600

    escape exceptions from Rake

--- all_the_tests.rb
+++ all_the_tests.rb
@@ -5,6 +5,6 @@ require_relative 'login_tests.rb'
 require_relative 'bookmarks_tests'
 require_relative 'feed_tests'
 
-# TODO: Swipe on cell with scrollView inside. Working with menu under the cell.(Bookmarks, downvote...).Not add in bookmark cell that alredy had added.
+# TODO: Swipe on cell with scrollView inside. Working with menu under the cell.(Bookmarks, downvote...).
 
 #out = %x`xcrun instruments -s devices` -save system calls into variable
