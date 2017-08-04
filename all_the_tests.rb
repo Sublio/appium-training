a979baa2aa4fac0d4b524147398f470448db8d66
Sublio <sublio1260@gmail.com>
2017-08-04 14:15:31 +0600

    require relative feed tests

--- all_the_tests.rb
+++ all_the_tests.rb
@@ -3,6 +3,7 @@
 require 'test/unit'
 require_relative 'login_tests.rb'
 require_relative 'bookmarks_tests'
+require_relative 'feed_tests'
 
 # TODO: Swipe on cell with scrollView inside. Working with menu under the cell.(Bookmarks, downvote...).Not add in bookmark cell that alredy had added.
 
